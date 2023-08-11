f821ab38dfd9a8fb3833bcfc3c4431c557b6a422
Hector <73690451+Hecariv@users.noreply.github.com>
2023-08-11 11:13:19 +0200

    Create test.java
    new file

---
+++ test.java
@@ -0,0 +1,28 @@
+private String parseMappingFile(final HttpServletRequest request, final MultipartFile mappingFile) throws FailureMappingExceptionC {
+    if (!MediaType.APPLICATION_JSON_VALUE.equals(mappingFile.getContentType())) {
+            final String errMsg = MessageFormatterUtil.format("The file '{}' is not a json file!", mappingFile.getOriginalFilename());
+            throw new FailureMappingExceptionC(errMsg, getResourceUri(request));
+    }
+    try {
+        return new String(mappingFile.getBytes());
+    } catch (IOException e) {
+       throw new FailureMappingExceptionC(e, getResourceUri(request));
+    }
+}
+
+private String test() {
+    return "this is a test";
+}
+
+
+// this is just a comment
+// wrng cment 
+// this is another comment
+
+private String hello() {
+    return "hello";
+}
+
+public String getName(String name) {
+    returns name;
+}
